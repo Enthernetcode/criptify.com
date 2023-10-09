cd885cd00532c0532a345ad791a6b809abd710ec
NAME <test@gmail.com>
2023-10-09 06:55:39 +0100

    .

--- userdashboard/restrict.js
+++ userdashboard/restrict.js
@@ -1,7 +1,32 @@
-const isLoggedIn = sessionStorage.getItem('user'));
-if (!isLoggedIn) {
-  window.location.replace('login.html');
-}
-else  {
+// const isLoggedIn = sessionStorage.getItem('user'));
+// if (!isLoggedIn) {
+//  window.location.replace('login.html');
+// }
+// else  {
  //nothing xup
-}
+// }
+
+//FUNCTION
+            function getUsername(){
+                 let keepLoggedIn = localStorage.getItem('keepLoggedIn')
+
+                 if(keepLoggedIn == 'yes'){
+                     currentUser = JSON.parse(localStorage.getItem('user'));
+                 }else{
+                      currentUser = JSON.parse(sessionStorage.getItem('user'));
+                 }
+            }
+
+            function signout(){
+                 sessionStorage.removeItem('user');
+                 localStorage.removeItem('user');
+                 localStorage.removeItem('keepLoggedIn');
+                 window.location="index.html";
+            }
+//WINDOW LOADS
+            window.onload = function(){
+                 getUsername();
+                 if(currentUser == null){
+                   window.location.replace('login.html');
+  }
+    }
